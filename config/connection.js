f61ba6d52b7b9e437aed089b06fa4bef29948753
Scripted Well <whigareda@theblackpilledpage.com>
2019-02-11 18:58:36 -0700

    updates to connection.js and server.js

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
 var mysql = require("mysql");
+var connection;
 
 //Creating connection to 'burgers_db'
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
-
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as ID: " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
 
 //exporting the connection
+connection.connect();
 module.exports = connection;
